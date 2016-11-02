c026df1f0ce5460e8416a1464b67c4000e67b4f9
Marcin Swinoga <marcin.swinoga@gmail.com>
2016-11-02 16:48:51 +0100

    Fix missing DOTFILE var

--- install.sh
+++ install.sh
@@ -2,17 +2,18 @@
 
 function linkFiles() {
   for FILE in $DOTDIR/*; do
+    DOTFILE=$HOME/.${FILE##*/}
     if [ $FILE != $0 ]; then
       if [ -e $DOTFILE ]; then
         mv $DOTFILE ${DOTFILE}.dotbak
       fi
-      DOTFILE=$HOME/.${FILE##*/}
       ln -s $FILE $DOTFILE
     fi
   done
 }
 
 DOTDIR=${0%/*}
+
 echo "Linking from $DOTDIR"
 linkFiles
 
