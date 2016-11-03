833ed5284e6625adb90d8b84ab78d4358b68e7ce
Marcin Swinoga <marcin.swinoga@gmail.com>
2016-11-03 11:12:00 +0100

    Add font installation

--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
-function linkFiles() {
+function installFonts {
+  CWD=$(pwd)
+  TMPDIR=$(mktemp -d)
+  git clone https://github.com/powerline/fonts $TMPDIR
+  if [ -d $TMPDIR ]; then
+    cd $TMPDIR
+    exec ./install.sh
+    cd $CWD
+    rm -rf $TMPDIR
+  fi
+}
+
+function linkFiles {
   for FILE in $DOTDIR/*; do
     DOTFILE=$HOME/.${FILE##*/}
     if [ $FILE != $0 ]; then
       if [ -e $DOTFILE ]; then
-        mv $DOTFILE ${DOTFILE}.dotbak
+#        mv $DOTFILE ${DOTFILE}.dotbak
+        echo "mv $DOTFILE ${DOTFILE}.dotbak"
       fi
-      ln -s $FILE $DOTFILE
+#      ln -s $FILE $DOTFILE
+      echo "ln -s $FILE $DOTFILE"
     fi
   done
 }
@@ -16,4 +30,5 @@ DOTDIR=${0%/*}
 
 echo "Linking from $DOTDIR"
 linkFiles
+#installFonts
 
